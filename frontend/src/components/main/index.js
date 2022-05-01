1f8b7ee518e74a35b205c85b676b48b8f0c3e193
m-triple-m <triplem656@gmail.com>
2022-05-01 12:21:36 +0530

    ..

--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -1,34 +1,14 @@
 import { AccountCircle, Dashboard } from "@mui/icons-material";
 import { Outlet } from "react-router-dom";
 
-
-
 import Header from "./header";
 
-
 const Main = () => {
-
-  const sidebarOptions = [
-    {
-      title : 'Profile',
-      icon : <AccountCircle/>,
-      link : '/user/profile'
-      },
-      {
-        title : 'Dashboard',
-        icon : <Dashboard />,
-        link : '/user/dashboard'
-        }
-  ]
-
   return (
     <>
       <Header />
       <Outlet />
       {/* <Footer /> */}
-      
-        
-      
     </>
   );
 };
